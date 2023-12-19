0a649d1668cd03a1b6a86b5b21aefaf1885711a9
Alexander Mueller <amueller@doctorcrank.de>
2023-12-19 22:10:23 +0100

    day4 successful

--- 2023/day4.py
+++ 2023/day4.py
@@ -4,4 +4,56 @@
 
 import utils
 
-print(utils.calc_sum([1,2,3,4]))
+def calculate_wins(card_game: dict) -> int:
+    wins = int()
+    for pulled_number in card_game['pulled']:
+        if pulled_number in card_game['winning']:
+            wins += 1
+    return wins
+
+def calculate_cards(card_data: dict) -> int:
+    sum_won_cards = 0
+    won_cards = list()
+    for card in card_data.keys():
+        sum_won_cards += 1
+        won_cards = calculate_wins(card_data[card])
+        for win in range(1, won_cards + 1):
+            card_data[card + win]['count'] += card_data[card]['count']
+            sum_won_cards += card_data[card]['count']
+
+    test = list()
+    for card in card_data.keys():
+        test.append(card_data[card]['count'])
+    print(utils.calc_sum(test))
+    return sum_won_cards
+
+def calculate_points(card_data: dict) -> int:
+    points = 0
+    for card in card_data.keys():
+        exponent = -1
+        for pulled_number in card_data[card]['pulled']:
+            if pulled_number in card_data[card]['winning']:
+                exponent += 1
+        points += int(pow(2, exponent))
+    return points
+
+def format_numbers(card_data_raw: list) -> dict:
+    formatted_numbers = dict()
+    for line in card_data_raw:
+        card_id = utils.get_id(line)
+        formatted_numbers[card_id] = {
+            'winning' : list(),
+            'pulled'  : list(),
+            'count'   : 1 }
+        for number in line.split(':')[1].split('|')[0].split():
+            formatted_numbers[card_id]['winning'].append(number)
+        for number in line.split(':')[1].split('|')[1].split():
+            formatted_numbers[card_id]['pulled'].append(number)
+    return formatted_numbers
+
+card_data_raw = utils.read_file('day4.txt')
+card_data = format_numbers(card_data_raw)
+part1_result = str(calculate_points(card_data))
+part2_result = str(calculate_cards(card_data))
+print('Part 1: ' + part1_result)
+print('Part 2: ' + part2_result)
--- 2023/utils.py
+++ 2023/utils.py
@@ -4,12 +4,15 @@ def read_file(path_file: str) -> list:
     input_data = list()
     file_input = open(path_file, 'r')
     for line in file_input.readlines():
-        input_data.append(line)
+        input_data.append(line.replace('\n', ''))
     file_input.close()
     return input_data
 
 def calc_sum(numbers: list) -> int:
     sum = int()
     for number in numbers:
-        sum += number
+        sum += int(number)
     return sum
+
+def get_id(line: str) -> str:
+    return int(line.split(':')[0].split(' ')[-1])
