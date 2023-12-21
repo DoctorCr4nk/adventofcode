fb6965f0b0ec3cdc14760843fed2a6ea5c7d1aaf
Alexander Müller <47869345+DoctorCr4nk@users.noreply.github.com>
2023-12-21 14:08:48 +0100

    Update day3.py
    day3 successful

--- 2023/day3.py
+++ 2023/day3.py
@@ -4,15 +4,15 @@
 
 import utils
 
-def check_adjacent(input_data: list, end_x: int, start_y: int, length: int) -> bool:
-    start_x = end_x - length
+def check_adjacent_symbols(input_data: list, start_x: int, end_x: int, start_y: int) -> bool:
     searchradius_x = 1
     searchradius_y = 1
     for y in range(start_y - searchradius_y , start_y + searchradius_y +1):
         if y < 0 or y > size['y']: continue
-        for x in range(start_x - searchradius_x, end_x + searchradius_x):
+        for x in range(start_x - searchradius_x, end_x + searchradius_x +1):
             if x < 0 or x > size['x']: continue
-            char = input_data[y][x]
+            try: char = input_data[y][x]
+            except IndexError: char = '.' # I do not understand
             if not (char.isdigit() or char == '.'):
                 return True
     return False
@@ -21,42 +21,51 @@ def get_adjacent_numbers(input_data: list, gear_x: int, gear_y: int) -> int:
     adjacent_numbers = []
     searchradius_x = 1
     searchradius_y = 1
-    for y in range(gear_y - searchradius_y , gear_y + searchradius_y +1):
+    y = gear_y - searchradius_y
+    while y <= (gear_y + searchradius_y):
         if y < 0 or y > size['y']: continue
-        for x in range(gear_x - searchradius_x, gear_x + searchradius_x +1):
+        x = gear_x - searchradius_x
+        while x <= (gear_x + searchradius_x):
             if x < 0 or x > size['x']: continue
             char = input_data[y][x]
             if char.isdigit():
-                print('Go left: ', end='')
-                for current_x in range(x, 0, -1):
-                    char = input_data[y][current_x]
-                    if char.isdigit():
-                        print(char, end='')
-                        left_x = current_x
-                    else: break
-                print('\nGo right: ', end='')
-                for current_x in range(x, size['x']):
-                    char = input_data[y][current_x]
-                    if char.isdigit():
-                        print(char, end='')
-                        right_x = current_x
-                    else: break
-                print('\nNumber: ' + str(input_data[y][left_x:right_x]))
-                adjacent_numbers.append(int(input_data[y][left_x:right_x]))
-
+                number = get_full_number(input_data, x, y)
+                adjacent_numbers.append(int(number['value']))
+                x = number['x'][-1]
+            x += 1
+        y += 1
     if len(adjacent_numbers) == 2:
-        print(adjacent_numbers)
         return adjacent_numbers[0] * adjacent_numbers[1]
     else:
-        print('------')
         return 0
+
+def get_full_number(input_data: list, digit_x: int, digit_y: int) -> dict:
+    number_data = {
+        'value' : str(),
+        'x'     : list(),
+        'y'     : digit_y }
+    if not input_data[digit_y][digit_x].isdigit():
+        print('Given coordinates is not a digit')
+        exit(1)
+    for x in range(0, len(input_data[digit_y])):
+        char = input_data[digit_y][x]
+        if char.isdigit():
+            number_data['x'].append(x)
+            number_data['value'] += char
+        else:
+            if digit_x in number_data['x']: break
+            else:
+                number_data['x'] = list()
+                number_data['value'] = str()
+    return number_data
 
 def get_gear_ratios(input_data: list) -> list:
     gear_ratios = list()
     y = 0
-    for line in input_data:
+    while y < len(input_data):
         x = 0
-        for char in line:
+        while x < len(input_data[y]):
+            char = input_data[y][x]
             if char == '*':
                 gear_ratios.append(get_adjacent_numbers(input_data, x, y))
             x += 1
@@ -69,35 +78,27 @@ def get_size(input_data: list):
         'x' : len(input_data[0]),
         'y' : len(input_data) }
 
-def get_valid_partnumbers(input_data: list) -> list:
+def get_partnumbers(input_data: list) -> list:
     partnumber = str()
     valid_partnumbers = list()
     y = 0
-    for line in input_data:
+    while y < size['y']:
         x = 0
-        for char in line:
+        while x < size['x']:
+            char = input_data[y][x]
             if char.isdigit():
-                partnumber += char
-            elif not len(partnumber) == 0:
-                if check_adjacent(input_data, x, y, len(partnumber)):
-                    valid_partnumbers.append(partnumber)
-                partnumber = ''
+                partnumber = get_full_number(input_data, x, y)
+                x = partnumber['x'][-1]
+                if check_adjacent_symbols(input_data, partnumber['x'][0], partnumber['x'][-1], y):
+                    valid_partnumbers.append(partnumber['value'])
             x += 1
-        if not len(partnumber) == 0 and check_adjacent(input_data, x-1, y, len(partnumber)):
-            valid_partnumbers.append(partnumber)
-        partnumber = ''
         y += 1
     return valid_partnumbers
 
-input_example = utils.read_file('input_data/day3.example.txt')
-input_data = utils.read_file('input_data/day3.txt')
-get_size(input_example)
-result1_e = utils.calc_sum(get_valid_partnumbers(input_example))
-result2_e = utils.calc_sum(get_gear_ratios(input_example))
-get_size(input_data)
-result1 = utils.calc_sum(get_valid_partnumbers(input_data))
-result2 = 0 #utils.calc_sum(get_gear_ratios(input_data))
-utils.print_results(str(result1_e),
-                    str(result1),
-                    str(result2_e),
-                    str(result2))
+results = list()
+for data_file in ['day3.example.txt', 'day3.txt']:
+    input_data = utils.read_file('input_data/' + data_file)
+    get_size(input_data)
+    results.append(utils.calc_sum(get_partnumbers(input_data)))
+    results.append(utils.calc_sum(get_gear_ratios(input_data)))
+utils.print_results(results)
