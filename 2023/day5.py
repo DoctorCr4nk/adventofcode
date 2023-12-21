973e80fe8337f73d9b501f286b17e41e5d9a8283
Alexander Mueller <amueller@doctorcrank.de>
2023-12-21 20:17:10 +0100

    fix ram issues

--- 2023/day5.py
+++ 2023/day5.py
@@ -10,40 +10,46 @@ def get_seed_ids() -> list:
         seed_ids.append(int(seed_id))
     return seed_ids
 
-def generate_translation(map: str) -> list:
+def generate_translation_map(map_type: str) -> list:
     read_data = False
-    translation = dict()
+    translation_map = list()
     for row in input_data:
-        if row.endswith(map + ' map:'):
-            read_data = True
-        elif row == '':
-            read_data = False
+        if row.endswith(map_type + ' map:'): read_data = True
+        elif row == '': read_data = False
         elif read_data:
             dest_start = int(row.split()[0])
             source_start = int(row.split()[1])
             range_length = int(row.split()[2])
-            for i in range(range_length):
-                translation[source_start+i] = dest_start+i
-    return translation
+            source_end = (source_start + range_length)
+            diff = (source_start - dest_start)
+            translation_map.append([source_start, source_end, diff])
+    return translation_map
+
+def translate(map_type: str, value: int) -> int:
+    translated_value = value
+    translation_map = generate_translation_map(map_type)
+    for translation in translation_map:
+        if value in range(translation[0], translation[1]):
+            translated_value = value - translation[2]
+    return translated_value
 
 global input_data
 results = list()
-for data_file in ['day5.example.txt']:
+for data_file in ['day5.example.txt', 'day5.txt']:
     input_data = utils.read_file('input_data/' + data_file)
     location = int()
     seed_id = int()
     seeds =  get_seed_ids()
     for seed in seeds:
+        print(str(seed_id+1) + '/' + str(len(seeds)), end=': ')
         source_value = seed
         for translation_type in ['soil', 'fertilizer', 'water', 'light', 'temperature', 'humidity', 'location']:
-            translation_map = generate_translation(translation_type)
-            try:
-                dest_value = translation_map[source_value]
-            except KeyError:
-                dest_value = source_value
+            print(end='.')
+            dest_value = translate(translation_type, source_value)
             if translation_type == 'location' and (location == 0 or location > dest_value):
                 location = dest_value
             source_value = dest_value
         seed_id += 1
+        print()
     results.append(location)
 utils.print_results(results)
